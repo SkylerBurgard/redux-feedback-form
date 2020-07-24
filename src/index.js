ae046eb89fc0968512c36953e74c6b83d166b78f
Skyler Burgard <skylerrburgard@hotmail.com>
2020-07-24 16:10:18 -0500

    Applied middleware, logger, and provider in index.js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,23 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
 import registerServiceWorker from './registerServiceWorker';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import logger from 'redux-logger';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+const feedbackReducer = (state = [], action) => {};
+
+const storeInstance = createStore(
+  combineReducers({
+    feedbackReducer,
+  }),
+  applyMiddleware(logger)
+);
+
+ReactDOM.render(
+  <Provider store={storeInstance}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
+// registerServiceWorker();
