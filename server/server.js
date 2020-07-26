c43fb3d10bac4ba16f3f27530bdc05fb7b1574e0
Skyler Burgard <skylerrburgard@hotmail.com>
2020-07-26 17:22:13 -0500

    Survey router set up

--- server/server.js
+++ server/server.js
@@ -5,13 +5,13 @@ const PORT = process.env.PORT || 5000;
 
 /** ---------- MIDDLEWARE ---------- **/
 app.use(bodyParser.json()); // needed for angular requests
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('build'));
 
 /** ---------- EXPRESS ROUTES ---------- **/
-
+app.use('/survey', surveyRouter);
 
 /** ---------- START SERVER ---------- **/
 app.listen(PORT, () => {
-    console.log('Listening on port: ', PORT);
+  console.log('Listening on port: ', PORT);
 });
