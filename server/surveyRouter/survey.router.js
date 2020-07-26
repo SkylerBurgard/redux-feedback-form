47be854da76548c03b94b57870cfe2b8df52c87e
Skyler Burgard <skylerrburgard@hotmail.com>
2020-07-26 16:42:10 -0500

    Starting router file, error in POST?

---
+++ server/surveyRouter/survey.router.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const router = express.Router();
+const pool = require('../modules/pool');
+
+router.post('/', (req, res) => {
+  let newSurvey = req.body;
+  let queryText = `INSERT INTO "feedback" ("feeling", "understanding", "support", "comments")
+    VALUES ($1, $2, $3, $4);`;
+
+  pool
+    .query(queryText, [
+      newSurvey.feeling,
+      newSurvey.understanding,
+      newSurvey.support,
+      newSurvey.comments,
+    ])
+    .then((result) => {
+      res.sendStatus(201);
+    });
+});
+
+router.get('/', (req, res) => {
+  let queryText = `SELECT * FROM "feedback" ORDER BY "id" DESC;`;
+  pool
+    .query(queryText)
+    .then((result) => {
+      res.sendStatus(result.rows);
+    })
+    .catch((error) => {
+      console.log('error in GET', error);
+      res.sendStatus(500);
+    });
+});
+
+module.exports = router;
